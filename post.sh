1d85520d793e75b5d9ac3382e92c2954d94e6c0f
Richie Cahill <richie@tmmworkshop.com>
2024-02-11 09:58:38 -0500

    test

--- post.sh
+++ post.sh
@@ -2,13 +2,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if [[ ! -v STATE_POST ]]; then
-  echo "DEPLOY_ENV is not set"
-elif [[ -z "$STATE_POST" ]]; then
-  echo "STATE_POST is set to the empty string"
-else
-  echo "STATE_POST has the value: $STATE_POST"
+  exit 0
 fi
-
 
 case $(uname -s) in
 MINGW64_NT?*)
